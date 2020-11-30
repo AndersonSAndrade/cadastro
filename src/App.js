59ebc740326f6cd1b968e912ec66b21241863315
andersons.andrade <asantos919@gmail.com>
2020-11-30 01:44:07 -0300

    Atualizando projeto

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bem Vindo ao Projeto React.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Dashboard
         </a>
       </header>
     </div>
